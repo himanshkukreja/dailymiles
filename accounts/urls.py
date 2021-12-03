7851b64944288a74e0c5d6856bdc919fb60eaead
Prince <princem.ec.19@nitj.ac.in>
2021-12-03 21:13:34 +0530

    made front end for accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='accounts'),
+    path('login', views.login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, HttpResponse, redirect
 
 def home(request):
     context = {
-        "title": "ML-TOOLBOX"
+        "title": "Daily Miles"
     }
     return render(request, 'accounts/home.html', context)
 
+def login(request):
+    context = {
+        "title": "Daily Miles",
+    }
+    return render(request, "accounts/login.html", context)
