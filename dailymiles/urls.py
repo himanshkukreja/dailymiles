9c22150c39e9eaaf587adb6249d76aef1ca025e4
Himanshu Kukreja <himansharora2001@gmail.com>
2021-12-03 22:05:12 +0530

    added feedback app

--- dailymiles/urls.py
+++ dailymiles/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("accounts.urls")),
+    path('', include("feedback.urls")),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
