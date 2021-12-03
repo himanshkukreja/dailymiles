08ae23ec4bb66868faaef5eb65a2a226355e911e
Prince <princem.ec.19@nitj.ac.in>
2021-12-03 22:32:11 +0530

    rectified mistake in urls in accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-urlpatterns = 
-[
+urlpatterns = [
     path('', views.home, name='accounts'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
