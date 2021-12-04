e4c197793382f6d0a180a31050adcffa5ae42096
Prince <princem.ec.19@nitj.ac.in>
2021-12-04 14:18:59 +0530

    made some changes in feeback app

--- dailymiles/urls.py
+++ dailymiles/urls.py
@@ -23,7 +23,9 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("accounts.urls")),
-    path('', include("feedback.urls")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
+    path('feedback', include("feedback.urls")),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':  settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT})
 ]
