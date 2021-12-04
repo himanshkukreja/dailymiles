fe5cda00e667855dd5b798de5421422f12c04c17
Prince <princem.ec.19@nitj.ac.in>
2021-12-04 15:29:16 +0530

    made changes in front end

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import feedbackmsg
-=======
 from .models import FeedbackMsg
->>>>>>> 62cf174b8e16e752cd909ee81a49440122f1c507
 
 # Register your models here.
 admin.site.register(FeedbackMsg)
