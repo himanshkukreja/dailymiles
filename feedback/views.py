ac7ad8e5b3133c1467017ed43aa66274266d1225
Prince <princem.ec.19@nitj.ac.in>
2021-12-04 14:31:16 +0530

    made changes in feedback

--- feedback/views.py
+++ feedback/views.py
@@ -1,25 +1,21 @@
 from django.contrib.messages import constants
 from django.shortcuts import render, HttpResponse, redirect
 import json
-from .models import feedbackmsg
+from .models import FeedbackMsg
 from django.contrib.auth.models import User
 from django.contrib import messages
 
 
 def feedback(request):
     if request.method == "POST":
-        username = request.POST.get("username")
-        contact = request.POST.get("contact")
+
+        if not request.user.is_authenticated():
+            return redirect("login")
+
         msg = request.POST.get("msg")
 
-        context = {
-            "username": username,
-            "contact": contact 
-        }
-
-        feedback_obj = User(username=username, contact=contact, msg=msg)
-
-        feedback_obj = feedbackmsg.objects.create(user=user_obj)
+        user = User.objects.get(pk=request.user)
+        feedback_obj = FeedbackMsg.objects.create(user=user, msg=msg)
         feedback_obj.save()
 
         return redirect("login")
