5a0e1482ff48cab4785fd0a9c5d3b6fb87c88bff
Himanshu Kukreja <himansharora2001@gmail.com>
2021-12-04 12:11:21 +0530

    added models for feedback

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# 1-> male
+# 0-> female
+
+class feedbackmsg(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.IntegerField(blank=True, null=True)
+    contact = models.FloatField(blank=True, null=True)
+    message = models.FloatField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.user.username
